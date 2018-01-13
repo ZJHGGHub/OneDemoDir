667641affc4b4acfa128d598736dff903b9f8bed
幻梦 <zhj13785@163.com>
2018-01-13 20:09:04 +0800

    第一次提交代码

---
+++ OneDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneDemo
+//
+//  Created by hzzj on 2018/1/13.
+//  Copyright © 2018年 hzzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OneDemo
+//
+//  Created by hzzj on 2018/1/13.
+//  Copyright © 2018年 hzzj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
